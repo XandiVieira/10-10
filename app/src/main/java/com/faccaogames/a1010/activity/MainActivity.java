096fe732a065b46a6abda600045af94b8e1e5cc7
XandiVieira <xandivieira@outlook.com>
2021-09-20 18:48:54 -0300

    initial structure

---
+++ app/src/main/java/com/faccaogames/a1010/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.faccaogames.a1010.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.faccaogames.a1010.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/faccaogames/a1010/model/User.java
@@ -0,0 +1,4 @@
+package com.faccaogames.a1010.model;
+
+public class User {
+}
