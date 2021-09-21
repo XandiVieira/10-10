2855c187362d31b9c123e56463581a35ff12e135
GabrielLeao <90697922+GabrielLeao02@users.noreply.github.com>
2021-09-20 23:20:46 -0300

    adicionei class user

--- app/src/main/java/com/faccaogames/a1010/model/User.java
+++ app/src/main/java/com/faccaogames/a1010/model/User.java
@@ -1,4 +1,10 @@
 package com.faccaogames.a1010.model;
 
 public class User {
+    String id;
+    String userName;
+    boolean premium;
+    String profilePhoto;
+    int power;
+    List<Experiment>experiments;
 }
