e39d170b6df0834927c478cb4174c2a340bd2792
Gabriel Leao <Gabriel.edg210@gmail.com>
2021-09-21 11:32:32 -0300

    update class Criteria, class user, class Experiment

--- app/src/main/java/com/faccaogames/a1010/model/Experiment.java
+++ app/src/main/java/com/faccaogames/a1010/model/Experiment.java
@@ -1,4 +1,16 @@
 package com.faccaogames.a1010.model;
 
 public class Experiment {
+    String userId;
+    StatusEnum status;
+    CategoryEnum category;
+    int valuations;
+    String contentPath;
+    List<Criteria>criteria;
+    List<String>comments;
+    List<String>alreadyRatedby;
+    List<Report>reports;
+    boolean commentsOn;
+    Long createdAt;
+
 }
